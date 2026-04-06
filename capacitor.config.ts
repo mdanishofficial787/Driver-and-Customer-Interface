953db4591b01af0b33a658b62008181bbb97195d
Adnan <48521@students.riphah.edu.pk>
2026-04-06 13:13:36 +0500

    Fix build output path to dist for deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ridenserve',
-  webDir: 'www',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
